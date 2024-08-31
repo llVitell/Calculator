d2dfc7129be193a04ef0026faf4036088b06ad40
unknown <omar.vite.a@uni.pe>
2024-08-31 17:14:51 -0500

    feat: renamed button's id

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.calculator
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var inputText = ""
+
+    private fun appendToInput(text: String) {
+        inputText += text
+        updateDisplay()
+    }
+
+    private fun updateDisplay() {
+        val display = findViewById<TextView>(R.id.display)
+        display.text = inputText
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val btnOne = findViewById<Button>(R.id.btnOne)
+        btnOne.setOnClickListener { appendToInput("1") }
+
     }
 }
